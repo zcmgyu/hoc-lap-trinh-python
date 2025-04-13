8884fbe3c0dc16ba9551e6f841049d5bafaa7441
zcmgyu <zcmgyu@gmail.com>
2025-04-13 20:22:20 +0900

    chore: arithmetic operator

---
+++ lesson_4/arithmetic-operator.py
@@ -0,0 +1,50 @@
+# Python Arithmetic Operators
+
+## Addition
+
+x1 = 10
+y1 = 20
+
+print("x1 + y1 = ", x1 + y1)
+
+## Subtraction
+
+x2 = 10
+y2 = 20
+
+print("x2 - y2 = ", x2 - y2)
+
+## Multiplication
+
+x3 = 10
+y3 = 20
+
+print("x3 * y3 = ", x3 * y3)
+
+## Division
+
+x4 = 10
+y4 = 20
+
+print("x4 / y4 = ", x4 / y4)
+
+## Modulus
+
+x5 = 7
+y5 = 2
+
+print("x5 % y5 = ", x5 % y5)
+
+## Exponentiation
+
+x6 = 2
+y6 = 3
+
+print("x6 ** y6 = ", x6 ** y6)
+
+## Floor Division
+
+x7 = 10
+y7 = 3
+
+print("x7 // y7 = ", x7 // y7)
