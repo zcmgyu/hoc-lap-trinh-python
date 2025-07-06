7c513425d4be3916dceee5acf4cfbc1fb7c16329
Long Nguyen <zcmgyu@gmail.com>
2025-07-06 13:58:39 +0900

    lesson5: range (#3)
    * lesson5: range
    
    * lesson5.2: loop

--- lesson_5/lists.py
+++ lesson_5/lists.py
@@ -34,3 +34,46 @@ print("Removing first item: ", list)
 # Remove item at index
 list.pop(1)
 print("Removing item at index 1: ", list)
+
+
+# Range of Indexes
+pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+reads = pages[0:5]
+
+print("Read pages: ", reads)
+
+todayPages = pages[5:]
+print("Today's pages: ", todayPages)
+
+# Changes a range of items
+newList = [10, 20, 30, 40, 50]
+newList[1:3] = [1, 2, 3, 4]
+print("New list: ", newList)
+
+# Insert a range of items
+danhSachLop = ["Nguyen Van A", "Nguyen Van B", "Nguyen Van C", "Nguyen Van D", "Nguyen Van E"]
+
+danhSachLop.insert(3, "Nguyen Dang Doanh")
+
+print("Danh sach lop: ", danhSachLop)
+
+# Extend a list
+lop6_3 = ["Nguyen Van A", "Nguyen Van B", "Nguyen Van C", "Nguyen Van D", "Nguyen Van E"]
+lop6_4 = ["Nguyen Van F", "Nguyen Van G", "Nguyen Van H", "Nguyen Van I", "Nguyen Van J"]
+
+print("Danh sach lop 6.3 BEFORE: ", lop6_3)
+lop6_3.extend(lop6_4)
+
+print("Danh sach lop AFTER: ", lop6_3)
+
+
+# Loop
+
+list = ['A', 'B', 'C', 'D', 'E']
+
+for item in list:
+    print(item + '+')
+
+
+# Chưa học xong while loop
